36dc434a1dfd749b44cf8f5f9f5ea82e73a48a75
Adam Bhouri <adam.bhouri@gmail.com>
2025-10-21 11:06:23 +0100

    Update NavigationContext.tsx

--- src/context/NavigationContext.tsx
+++ src/context/NavigationContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, ReactNode } from 'react'
+import React, { createContext, useContext } from 'react'
 import { Home, FileText, Users, Newspaper, DollarSign } from 'lucide-react'
 
 export interface NavigationItem {
